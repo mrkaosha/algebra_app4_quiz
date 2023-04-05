85d8fe6be08f3a3d9d41bd0a258d3984c5515da7
Mr. Kao <mrkaosha@gmail.com>
2023-04-05 12:28:35 -0700

    formatted calculate_slope

--- lib/calculate_slope.dart
+++ lib/calculate_slope.dart
@@ -331791,9 +331791,14 @@ class CalculateSlope {
 
   CalculateSlope() {
     for (int i = 0; i < num.length; i++) {
-      if (num[i] * x1[i] % den[i] == 0)
-        parameters
-            .add({'num': num[i], 'den': den[i], 'x1': x1[i], 'y1': y1[i]});
+      if (num[i] * x1[i] % den[i] == 0) {
+        parameters.add({
+          'num': num[i],
+          'den': den[i],
+          'x1': x1[i],
+          'y1': y1[i],
+        });
+      }
     }
     parameters.shuffle();
   }
